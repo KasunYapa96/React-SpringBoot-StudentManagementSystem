7a81a9abf17811dfca213890e3c8e2d9b36c0ebb
KasunYapa96 <kasunishankayapa1996@gmail.com>
2020-10-28 16:25:36 +0530

    Initializing-to-frontend&backend

---
+++ student-management-backend/src/main/java/com/student/management/studentmanagementweb/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.student.management.studentmanagementweb.controller;
+
+import com.student.management.studentmanagementweb.model.Student;
+import com.student.management.studentmanagementweb.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("/Students")
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+
+
+}
